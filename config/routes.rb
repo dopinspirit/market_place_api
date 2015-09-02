42b37602463b5aff47a51099b1b9a8dea8ed35a3
Oleksandr Povshyk <xtwinpeaks@gmail.com>
2015-09-02 18:04:32 +0300

    Adds users endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
   devise_for :users
+  
   namespace :api, defaults: { format: :json },
                               constrains: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constrains: ApiConstraints.new(version: 1, default: true) do
-
+      resources :users, :only => [:show]
     end
   end
 
