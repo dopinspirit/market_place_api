df8eee80127f764e5f8e7394ea2a325b2f8321e7
Oleksandr Povshyk <xtwinpeaks@gmail.com>
2015-09-02 13:31:05 +0300

    versioning with tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json },
                               constrains: { subdomain: 'api' }, path: '/' do
-    
+    scope module: :v1, constrains: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
 
 end
