afca8d41e92976462fa4e20c78a2b575cee5cd9f
dopinspirit <xtwinpeaks@gmail.com>
2015-09-03 21:17:23 +0300

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :api, defaults: { format: :json },
-                              constrains: { subdomain: 'api' }, path: '/' do
-    scope module: :v1, constrains: ApiConstraints.new(version: 1, default: true) do
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show]
     end
   end
 
-  
+
 end
